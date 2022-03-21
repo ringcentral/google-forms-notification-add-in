a193d0ff9df1ac04db667c8f854ac87cdd27a4f8
Embbnux Ji <embbnux@embbnux.com>
2022-03-21 18:31:10 +0800

    chore: add view test

---
+++ tests/view.test.js
@@ -0,0 +1,28 @@
+const request = require('supertest');
+
+const { server } = require('../src/server');
+
+describe('Setup', () => {
+  it('should get 200 when visit setup page', async () => {
+    const res = await request(server).get('/setup');
+    expect(res.status).toEqual(200);
+    expect(res.text).toContain('window.clientConfig');
+    expect(res.text).toContain(`${process.env.ASSETS_PATH}/app.js`);
+  });
+
+  it('should get 200 when visit home page', async () => {
+    const res = await request(server).get('/home');
+    expect(res.status).toEqual(200);
+  });
+
+  it('should get 200 when visit home page', async () => {
+    const res = await request(server).get('/');
+    expect(res.status).toEqual(200);
+  });
+
+  it('should get 200 when GOOGLE_SITE_VERIFICATION_TOKEN is set', async () => {
+    const res = await request(server).get(`/${process.env.GOOGLE_SITE_VERIFICATION_TOKEN}.html`);
+    expect(res.status).toEqual(200);
+    expect(res.text).toContain(`google-site-verification: ${process.env.GOOGLE_SITE_VERIFICATION_TOKEN}.html`);
+  });
+});
